5705508e1d7339485afde85c13639cb1dcab825b
jpeek01 <jpeek01@gmail.com>
2018-08-04 19:41:14 -0500

    adding code to dynamically add the pcitures from yelp

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -3,30 +3,16 @@
     var modal = document.getElementById('myModal');
     
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    // var btn = document.getElementById("myBtn");
     
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
     
     // When the user clicks on the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-        $('#pictureDiv').slick('unslick');
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-            $('#pictureDiv').slick('unslick');
-        }
-    }
-    /// picture div for modal 
+    // dynMarker.onclick = function() {
+    //     modal.style.display = "block";
+    // }
+        /// picture div for modal 
     $(document).ready(function () {
         $('#pictureDiv').slick({
             fade: false,
@@ -34,4 +20,19 @@
             arrows: false
         });
     });
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+        // $('#pictureDiv').slick('unslick');
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            // $('#pictureDiv').slick('unslick');
+        }
+    }
+
 // 
