e06b55cdf574ca47a7dadd5b2edfd51d3a74ca97
Donna Okrongly <donna.okrongly@gmail.com>
2018-11-27 13:05:17 -0600

    deploy attempt 4

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/googlebaeb2747edb6a20f.html", function(req, res) {
-  res.sendFile(path.join(__dirname, "googlebaeb2747edb6a20f.html"));
+  res.sendFile(path.join(__dirname, "./googlebaeb2747edb6a20f.html"));
 });
 
 app.listen(PORT, function() {
